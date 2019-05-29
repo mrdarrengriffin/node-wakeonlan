2bd23aa228798228494c38e59e640337a6df878e
Darren Griffin <darren.griffin@live.co.uk>
2019-05-29 19:36:27 +0100

    Added HTTPS for public usage

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const { exec } = require('child_process');
 const express = require('express')
 const app = express();
 var fs = require('fs')
+var https = require('https')
+
+const options = {
+    key: fs.readFileSync(__dirname+'/cloudflare.key'),
+    cert: fs.readFileSync(__dirname+'/cloudflare.pem')
+  };
 
 var config = JSON.parse(fs.readFileSync(__dirname+'/config.json').toString())
-app.listen(8080);
+
+https.createServer(options,app).listen(8080)
 
 app.get('/wake',(req, res, next) => {
     exec(`wakeonlan -i ${config.host} -p ${config.port} ${config.mac_address}`, (err, stdout, stderr) => {
