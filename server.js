93e621209387bef10229328608a42a18c87d642b
Darren Griffin <darren.griffin@live.co.uk>
2019-05-29 19:58:59 +0100

    Moved code

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ var fs = require('fs')
 var https = require('https')
 
 const options = {
-    key: fs.readFileSync(__dirname+'/cloudflare.key'),
-    cert: fs.readFileSync(__dirname+'/cloudflare.pem')
+    key: fs.readFileSync('./cloudflare.key'),
+    cert: fs.readFileSync('./cloudflare.pem')
   };
 
 var config = JSON.parse(fs.readFileSync(__dirname+'/config.json').toString())
 
-https.createServer(options,app).listen(8080)
 
 app.get('/wake',(req, res, next) => {
     exec(`wakeonlan -i ${config.host} -p ${config.port} ${config.mac_address}`, (err, stdout, stderr) => {
@@ -19,3 +18,4 @@ app.get('/wake',(req, res, next) => {
     })
 })
 
+https.createServer(options,app).listen(8080)
