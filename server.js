592d6b87e5643b99d14ad95cd442f1e3cc69408f
Darren Griffin <darren.griffin@live.co.uk>
2019-05-29 19:13:13 +0100

    Added http listener

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const { exec } = require('child_process');
+const express = require('express')
+const app = express();
 var fs = require('fs')
 
 var config = JSON.parse(fs.readFileSync(__dirname+'/config.json').toString())
+app.listen(8080);
 
-exec(`wakeonlan -i ${config.host} -p ${config.port} ${config.mac_address}`, (err, stdout, stderr) => {
-    console.log(stdout)
+app.get('/wake',(req, res, next) => {
+    exec(`wakeonlan -i ${config.host} -p ${config.port} ${config.mac_address}`, (err, stdout, stderr) => {
+        res.json({err,stdout,stderr})
+    })
 })
+
